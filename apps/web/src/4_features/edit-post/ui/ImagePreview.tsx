33d1b4d4669314d113643c61be7f6c528e7d06a3
JaeHye0k <55015406+JaeHye0k@users.noreply.github.com>
2025-10-12 09:58:45 +0900

    fix: image 컴포넌트 fill 속성 추가

--- apps/web/src/4_features/edit-post/ui/ImagePreview.tsx
+++ apps/web/src/4_features/edit-post/ui/ImagePreview.tsx
@@ -9,7 +9,7 @@ export const ImagePreview = ({ previewImage, handleDeleteFile }: ImagePreviewPro
     return (
         <div className='absolute flex gap-x-[9px] bottom-16 z-10'>
             <div className='relative size-[6rem]'>
-                <Image src={previewImage} alt='썸네일 미리보기' className='object-cover w-full h-full' />
+                <Image src={previewImage} alt='썸네일 미리보기' className='object-cover' fill />
                 <button
                     type='button'
                     onClick={() => handleDeleteFile()}
